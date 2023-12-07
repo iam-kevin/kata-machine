c90687404fb8e42c6929ab70c53aaaa4dc41bc10
Kevin James <54111777+iam-kevin@users.noreply.github.com>
2023-12-08 01:19:32 +0300

    add alternative: primeWalk

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,8 +1,3 @@
-// base:
-// 1. seen it
-
-const visited = [];
-
 const isSame = (a: Point, b: Point) => a.x === b.x && a.y === b.y;
 
 export default function solve(
@@ -18,7 +13,6 @@ export default function solve(
         point.y < 0 ||
         point.y > maze.length;
 
-    const uniqIndex = (point: Point) => point.y * 10 + point.x;
     const points: Point[] = [];
     const visited: number[] = [];
 
@@ -50,10 +44,81 @@ export default function solve(
                 }
             }
         }
-
         return isEnded;
     };
 
-    walk(start, visited);
+    // walk(start, visited);
+    primeWalk(maze, wall, start, end, visited, points);
     return points;
 }
+
+// ----
+// functional walk /
+// ----
+
+const isWall = (maze: string[], wall: string, point: Point) =>
+    maze[point.y].charAt(point.x) === wall;
+const isOutOfBounds = (maze: string[], point: Point) =>
+    point.x < 0 ||
+    point.x >= maze[0].length ||
+    point.y < 0 ||
+    point.y > maze.length;
+
+type UniqIndex = number;
+const uniqIndex = (point: Point): UniqIndex => point.y * 10 + point.x;
+
+type Maze = string[];
+type Wall = string;
+const primeWalk = function (
+    maze: Maze,
+    wall: Wall,
+    curr: Point,
+    end: Point,
+    visited: UniqIndex[],
+    path: Point[],
+) {
+    if (
+        isOutOfBounds(maze, curr) ||
+        isWall(maze, wall, curr) ||
+        visited.includes(uniqIndex(curr))
+    )
+        return false;
+
+    // viisting things
+    visited.push(uniqIndex(curr));
+    path.push(curr);
+
+    if (isSame(curr, end)) {
+        return true;
+    }
+
+    for (let i = 0; i < dirs.length; i++) {
+        const [dx, dy] = dirs[i];
+        if (
+            primeWalk(
+                maze,
+                wall,
+                {
+                    x: curr.x + dx,
+                    y: curr.y + dy,
+                },
+                end,
+                visited,
+                path,
+            )
+        ) {
+            return true;
+        }
+    }
+
+    // if false remove
+    path.pop();
+    return false;
+};
+
+const dirs = [
+    [-1, 0],
+    [0, -1],
+    [1, 0],
+    [0, 1],
+];
