762d21cda0dd984a11a34f35e7b46c08450e0832
Kevin James <54111777+iam-kevin@users.noreply.github.com>
2023-12-07 18:25:06 +0300

    impl: Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,47 @@
+type Node<T> = {
+    prev: Node<T> | null;
+    value: T;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private top: Node<T> | null;
 
     constructor() {
+        this.length = 0;
+        this.top = null;
     }
 
     push(item: T): void {
+        const node: Node<T> = { value: item, prev: null };
+        if (this.top === null) {
+            this.top = node;
+        } else {
+            node.prev = this.top;
+            this.top = node;
+        }
 
-}
+        this.length += 1;
+    }
     pop(): T | undefined {
+        if (this.top === null) {
+            return undefined;
+        }
 
-}
+        const node = this.top;
+        this.top = this.top.prev;
+
+        node.prev = null; // 'free'-ing memory
+        this.length -= 1;
+
+        return node.value;
+    }
+
     peek(): T | undefined {
+        if (this.top === null) {
+            return undefined;
+        }
 
-}
+        return this.top.value;
+    }
 }
