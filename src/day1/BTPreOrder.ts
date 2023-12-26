90f4cc5df5c1c9fd621145815aa82a042ee5f5c4
Kevin James <54111777+iam-kevin@users.noreply.github.com>
2023-12-26 10:26:48 +0300

    feat: dfs walks

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,17 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(node: BinaryNode<number>, vals: number[]): number[] {
+    vals.push(node.value);
 
+    if (node.left) {
+        walk(node.left, vals);
+    }
+
+    if (node.right) {
+        walk(node.right, vals);
+    }
+
+    return vals;
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
