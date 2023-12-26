78967f73fa4a5275e581f1392c50d9d60533c150
Kevin James <54111777+iam-kevin@users.noreply.github.com>
2023-12-26 14:54:53 +0300

    bfs

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,19 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q = [head];
 
+    while (q.length > 0) {
+        const node = q.shift();
+
+        if (node) {
+            if (node.value === needle) {
+                return true;
+            }
+
+            // add to queue
+            if (node.left) q.push(node.left);
+            if (node.right) q.push(node.right);
+        }
+    }
+
+    return false;
 }
